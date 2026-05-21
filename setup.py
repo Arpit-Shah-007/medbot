4b0f9f75708eb4e54562bacb8a134ac88641a2a5
Arpit Shah <ashah10@stevens.edu>
2026-05-21 17:22:54 -0400

    Initialize Phase II - project structure with necessary directories and files.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Arpit Shah",
+    author_email="ashah10@stevens.edu",
+    packages=find_packages(),
+    install_requires=[]
+)
